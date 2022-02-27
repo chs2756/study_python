b88e89e7dbdf68a062cd6a62fcc5919ad94a3684
chs2756 <poma36@naver.com>
2022-02-27 21:08:21 +0900

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,20 @@
+'''Zip Function'''
+numbers = [1, 2, 3]
+letters = ["A", "B", "C"]
+for pair in zip(numbers, letters):
+  print(pair)
 
+#(1, 'A')
+#(2, 'B')
+#(3, 'C')
+
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant[-1]
+
+  
